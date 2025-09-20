f4da94d8d989b484e67de72f07e0c391d19d2ab8
이진 <ctce7226@gmail.com>
2025-09-20 11:32:22 +0900

    feat: RDP, 칼만필터, 비스왈랑감, 평균보간법 알고리즘 적용

--- coordinate_processor.py
+++ coordinate_processor.py
@@ -8,47 +8,6 @@ from pykalman import KalmanFilter
 from pyproj import Transformer
 transformer = Transformer.from_crs('EPSG:4326', 'EPSG:5179')
 
-# 평균 보간법
-def smooth_gps_with_average_time(records: list) -> List[Dict[str, Any]]:
-    
-    smoothed_records = []
-    for i in range(2, len(records), 3):
-        avg_lat = (records[i - 2]['lat'] + records[i - 1]['lat'] + records[i]['lat']) / 3
-        avg_lng = (records[i - 2]['lng'] + records[i - 1]['lng'] + records[i]['lng']) / 3
-        smoothed_records.append({
-            'lat': avg_lat,
-            'lng': avg_lng
-        })
-    return smoothed_records
-
-def smooth_gps_with_average_distance(points, target_distance=3):
-    filtered = [points[0]]
-    last_point = points[0]
-    
-    for point in points[1:]:
-        distance = haversine_distance(
-            last_point['lat'], last_point['lng'],
-            point['lat'], point['lng']
-        )
-        if distance >= target_distance:
-            filtered.append(point)
-            last_point = point
-    
-    return filtered
-            
-
-def haversine_distance(lat1, lon1, lat2, lon2):
-    R = 6371000
-    phi1 = np.radians(lat1)
-    phi2 = np.radians(lat2)
-    delta_phi = np.radians(lat2 - lat1)
-    delta_lambda = np.radians(lon2 - lon1)
-    
-    a = np.sin(delta_phi/2)**2 + np.cos(phi1) * np.cos(phi2) * np.sin(delta_lambda/2)**2
-    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
-    return R * c
-    
-
 # Kalman Filter 알고리즘
 def smooth_gps_with_kalman(records: list) -> tuple:
     measurements = np.array([[r['lat'], r['lng']] for r in records])
@@ -74,68 +33,98 @@ def smooth_gps_with_kalman(records: list) -> tuple:
     
     return smoothed_records, measurements, smoothed_states_means
 
+
+# 평균 보간법
+def smooth_gps_with_average_time(records: list) -> List[Dict[str, Any]]:
+    
+    smoothed_records = []
+    for i in range(2, len(records), 3):
+        avg_lat = (records[i - 2]['lat'] + records[i - 1]['lat'] + records[i]['lat']) / 3
+        avg_lng = (records[i - 2]['lng'] + records[i - 1]['lng'] + records[i]['lng']) / 3
+        smoothed_records.append({
+            'lat': avg_lat,
+            'lng': avg_lng
+        })
+    return smoothed_records
+
+def smooth_gps_with_average_distance(points, target_distance=3):
+    
+    utm_points = to_utm(points)
+    
+    filtered = [utm_points[0]]
+    last_point = utm_points[0]
+    for point in utm_points[1:]:
+        distance = pythagoras_distance(
+            last_point['y'], last_point['x'],
+            point['y'], point['x']
+        )
+        if distance >= target_distance:
+            filtered.append(point)
+            last_point = point
+    
+    return filtered
+
+def pythagoras_distance(lat1, lon1, lat2, lon2):
+    return ((lat2 - lat1)**2 + (lon2 - lon1)**2)**0.5
+
+
+# UTM 좌표계로 변환
+def to_utm(points):
+    utm_points = []
+    for i, p in enumerate(points):
+        y, x = transformer.transform(p['lat'], p['lng'])
+        utm_points.append({'x': x, 'y': y, 'original_index': i})
+    return utm_points
+
+
 # 비스발링감-와이엇 알고리즘
 def simplify_with_visvalingam(points: List[Dict[str, Any]], threshold_area: int) -> List[Dict[str, Any]]:
     if len(points) < 3 or not transformer:
         return points
 
-    # UTM 변환
-    utm_points = []
-    for i, p in enumerate(points):
-        x, y = transformer.transform(p['lat'], p['lng'])
-        utm_points.append({'x': x, 'y': y, 'original_index': i})
+    utm_points = to_utm(points)
 
-    simplified_utm_points = list(utm_points)
-
-    while len(simplified_utm_points) > 2:
+    while len(utm_points) > 2:
         min_area = float('inf')
         min_index = -1
 
-        # 연속된 세 점의 면적에서 가장 작은 면적 계산
-        for i in range(1, len(simplified_utm_points) - 1):
+        for i in range(1, len(utm_points) - 1):
             area = calculate_utm_area(
-                simplified_utm_points[i - 1],
-                simplified_utm_points[i],
-                simplified_utm_points[i + 1]
+                utm_points[i - 1],
+                utm_points[i],
+                utm_points[i + 1]
             )
             if area < min_area:
                 min_area = area
                 min_index = i
 
-        # 임계값 보다 크다면 끝
         if min_area >= threshold_area:
             break
         
         if min_index != -1:
-            simplified_utm_points.pop(min_index)
+            utm_points.pop(min_index)
         else:
             break
     
-    # 4. 단순화가 끝난 후, 저장해둔 'original_index'를 사용하여 원본 포인트 리스트에서 최종 결과물을 생성합니다.
-    final_points = [points[p['original_index']] for p in simplified_utm_points]
+    final_points = [points[p['original_index']] for p in utm_points]
     return final_points
 
-# 삼각형 면적 계산
 def calculate_utm_area(p1: Dict, p2: Dict, p3: Dict) -> float:
     return 0.5 * abs(
-        p1['x'] * (p2['y'] - p3['y']) +
-        p2['x'] * (p3['y'] - p1['y']) +
-        p3['x'] * (p1['y'] - p2['y'])
+        p1['y'] * (p2['x'] - p3['x']) +
+        p2['y'] * (p3['x'] - p1['x']) +
+        p3['y'] * (p1['x'] - p2['x'])
     )
+
 
 # 비스발링감-와이엇 알고리즘, 최소힙 사용
 def simplify_with_visvalingam_min_heap(points: List[Dict[str, Any]], threshold_area: int) -> List[Dict[str, Any]]:
     if len(points) < 3 or not transformer:
         return points
 
-    # UTM 변환
-    utm_points = []
-    for i, p in enumerate(points):
-        x, y = transformer.transform(p['lat'], p['lng'])
-        utm_points.append({'x': x, 'y': y, 'original_index': i})
+    utm_points = to_utm(points)
 
-    # 힙 구성
-    heap = []
+    heap = []           # 힙 구성
     for i in range(1, len(utm_points)-1):
         area = calculate_utm_area(
                 utm_points[i - 1],
@@ -144,58 +133,46 @@ def simplify_with_visvalingam_min_heap(points: List[Dict[str, Any]], threshold_a
         )
         heapq.heappush(heap, (area, i-1, i, i+1))
     
-    # 각 점의 활성화 상태
-    is_active = [True] * len(utm_points)
-    
-    # 이전/다음 노드의 정보
-    prev_nodes = [None] + [i-1 for i in range(1, len(utm_points))]
+    is_active = [True] * len(utm_points)            # 각 점의 활성화 상태
+    prev_nodes = [None] + [i-1 for i in range(1, len(utm_points))]          # 이전/다음 노드의 정보
     next_nodes = [i+1 for i in range(len(utm_points)-1)] + [None]
     
-    # 계산
     while heap:
         area, start, mid, end = heapq.heappop(heap)
-        # 비활성화된 점이 있다면 continue
-        if not is_active[mid] or prev_nodes[mid] != start or next_nodes[mid] != end:
+        if not is_active[mid] or prev_nodes[mid] != start or next_nodes[mid] != end:        # 비활성화 점, 변경된 점 스킵
             continue
-        # 임계치 체크
         if area < threshold_area:
-            # 가운데 인덱스 비활성화, 주변 노드 정보 갱신
-            is_active[mid] = False
+            is_active[mid] = False                  # 가운데 인덱스 비활성화, 주변 노드 정보 갱신
             next_nodes[start] = next_nodes[mid]
             prev_nodes[end] = prev_nodes[mid]
-            # 왼쪽 삼각형
-            if prev_nodes[start] is not None:
+            if prev_nodes[start] is not None:       # 왼쪽 삼각형
                 area1 = calculate_utm_area(
                     utm_points[prev_nodes[start]],
                     utm_points[start],
                     utm_points[end]
                 )
                 heapq.heappush(heap, (area1, prev_nodes[start], start, end))
-            # 오른쪽 삼각형
-            if next_nodes[end] is not None:
+            if next_nodes[end] is not None:         # 오른쪽 삼각형
                 area2 = calculate_utm_area(
                     utm_points[start],
                     utm_points[end],
                     utm_points[next_nodes[end]]
                 )
-                # 힙 튜플 형식 통일
                 heapq.heappush(heap, (area2, start, end, next_nodes[end]))
         else:
             break
     
-    # 활성화되어 있는 점만 뽑아내기
-    result = []
+    result = []         # 활성화되어 있는 점만 뽑아내기
     for idx in range(len(is_active)):
         if is_active[idx]:
             result.append(points[idx])
     return result
 
-# 모두 active 인지 검사
 def is_all_active(is_active, start, mid, end):
     return is_active[start] and is_active[mid] and is_active[end]
 
 
-# 원본 러닝 데이터 업로드
+# 데이터 로드/저장
 def load_jsonl_data(file_path: str) -> List[Dict[str, Any]]:
     records = []
     try:
@@ -216,7 +193,6 @@ def load_jsonl_data(file_path: str) -> List[Dict[str, Any]]:
         print(f"[오류] 파일 처리 중 예기치 않은 오류가 발생했습니다: {e}", file=sys.stderr)
     return records
 
-# 보간한 데이터 저장
 def save_data_to_jsonl(data: List[Dict[str, Any]], file_path: str):
     try:
         with open(file_path, 'w', encoding='utf-8') as f:
@@ -226,6 +202,7 @@ def save_data_to_jsonl(data: List[Dict[str, Any]], file_path: str):
         print(f"\n[성공] 데이터가 '{file_path}' 파일에 성공적으로 저장되었습니다.")
     except Exception as e:
         print(f"\n[오류] 파일 저장 중 오류가 발생했습니다: {e}", file=sys.stderr)
+
 
 # Main
 if __name__ == "__main__":
@@ -237,15 +214,17 @@ if __name__ == "__main__":
     running_data = load_jsonl_data(input_file)
     
     # Kalman Filter를 활용하여 보간 및 저장
-    smoothed_data, original_measurements, smoothed_measurements = smooth_gps_with_kalman(running_data)
-    save_data_to_jsonl(smoothed_data, os.path.join(script_dir, "result/kalman_filtered_data.jsonl"))
+    kalman_data, original_measurements, smoothed_measurements = smooth_gps_with_kalman(running_data)
+    save_data_to_jsonl(kalman_data, os.path.join(script_dir, "result/kalman_filtered_data.jsonl"))
     
-    print("원본 : " + str(len(smoothed_data)))
+    # 원본
+    print("\n========== 원본 ==================\n")
+    print("알고리즘 적용 전 : " + str(len(kalman_data)))
     
     # 평균 보간법
     print("\n========== 평균보간법 알고리즘 ==================\n")
     start_time = time.time()
-    average_data = smooth_gps_with_average_time(smoothed_data)
+    average_data = smooth_gps_with_average_time(kalman_data)
     average_data = smooth_gps_with_average_distance(average_data)
     print("평균보간법 적용 후 : " + str(len(average_data)))
     print(f"평균보간법 처리 시간: {time.time() - start_time:.4f}초")
@@ -255,16 +234,16 @@ if __name__ == "__main__":
     
     print("\n========== 기본 비스발링감-와이엇 알고리즘 ==================\n")
     start_time = time.time()
-    vis_data = simplify_with_visvalingam(smoothed_data, threshold_area)
+    vis_data = simplify_with_visvalingam(kalman_data, threshold_area)
     print("비스발링감-와이엇 알고리즘 적용 후 : " + str(len(vis_data)))
     print(f"비스발링감-와이엇 알고리즘 처리 시간: {time.time() - start_time:.4f}초")
     
     print("\n========== 최소힙 비스발링감-와이엇 알고리즘 ==================\n")
     start_time = time.time()
-    vis_data_heap = simplify_with_visvalingam_min_heap(smoothed_data, threshold_area)
+    vis_data_heap = simplify_with_visvalingam_min_heap(kalman_data, threshold_area)
     print("비스발링감-와이엇 알고리즘 적용 후 : " + str(len(vis_data_heap)))
     print(f"비스발링감-와이엇 알고리즘 처리 시간: {time.time() - start_time:.4f}초")
     
     
     # 보간한 데이터 저장
-    save_data_to_jsonl(smoothed_data, output_file)
+    save_data_to_jsonl(vis_data_heap, output_file)
--- coordinate_rdp_processor.py
+++ coordinate_rdp_processor.py
@@ -1,190 +1,165 @@
-from typing import List, Dict, Any
+import numpy as np
 import math
 import sys
 import os
-import pyproj
 import json
 import time
 
+from pykalman import KalmanFilter
+from typing import List, Dict, Any
+from pyproj import Transformer
+transformer = Transformer.from_crs('EPSG:4326', 'EPSG:5179')
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, idx):
         self.x = x
         self.y = y
+        self.idx = idx
 
     def __repr__(self):
-        return f"Point(x={self.x:.6f}, y={self.y:.6f})"
+        return f"Point(x={self.x:.6f}, y={self.y:.6f}, idx={self.idx})"
 
 
-class RDPPointProcessor:
-    # WGS84 좌표계 정의
-    def __init__(self):
-        self.crs_wgs84 = pyproj.CRS("EPSG:4326")
+# Kalman Filter 알고리즘
+def smooth_gps_with_kalman(records: list) -> tuple:
+    measurements = np.array([[r['lat'], r['lng']] for r in records])
+    transition_matrix = [[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]]
+    observation_matrix = [[1, 0, 0, 0], [0, 1, 0, 0]]
+    initial_state_mean = [measurements[0, 0], measurements[0, 1], 0, 0]
 
-
-    # GPS 단순화
-    # lat_lon_points : 프론트엔드에서 받은 GPS 좌표 리스트, epsilon : 허용 오차 (미터 단위)
-    def process_gps_track(self, lat_lon_points: list[Point], epsilon: float) -> list[Point]:
-        if not lat_lon_points or len(lat_lon_points) < 2:
-            return lat_lon_points
-
-        utm_points, utm_crs = self._convert_to_utm(lat_lon_points)
-        simplified_utm_points = self._rdp_simplify(utm_points, epsilon)
-        simplified_lat_lon_points = self._convert_to_wgs84(simplified_utm_points, utm_crs)
-
-        return simplified_lat_lon_points
-      
-    # UTM 좌표계로 변환
-    def _get_utm_crs(self, lat: float, lon: float) -> pyproj.CRS:
-        # 경도를 이용해 UTM Zone 번호를 계산합니다. (1~60)
-        zone_number = math.floor((lon + 180) / 6) + 1
-        
-        # 위도를 이용해 남반구인지 북반구인지 결정
-        # 북반구는 326xx, 남반구는 327xx EPSG 코드를 사용합니다.
-        epsg_base = 32600 if lat >= 0 else 32700
-        
-        # 최종 EPSG 코드를 조합하여 CRS 객체를 생성합니다.
-        epsg_code = f"EPSG:{epsg_base + zone_number}"
-        return pyproj.CRS(epsg_code)
-
-    # 위경도 -> UTM
-    def _convert_to_utm(self, lat_lon_points: list[Point]) -> tuple[list[Point], pyproj.CRS]:
-        # UTM Zone 설정
-        first_point = lat_lon_points[0]
-        utm_crs = self._get_utm_crs(first_point.x, first_point.y)
-
-        # WGS84 -> UTM 변환을 위한 Transformer 생성
-        transformer = pyproj.Transformer.from_crs(self.crs_wgs84, utm_crs, always_xy=True)
-
-        # 변환된 UTM 좌표 리스트 생성
-        utm_points = []
-        for point in lat_lon_points:
-            utm_x, utm_y = transformer.transform(point.y, point.x)
-            utm_points.append(Point(utm_x, utm_y))
-        
-        return utm_points, utm_crs
-
-
-    # RDP 알고리즘을 사용하여 GPS 좌표 단순화
-    def _rdp_simplify(self, points: list[Point], epsilon: float) -> list[Point]:
-        if len(points) < 3:
-            return points
-
-        start_point = points[0]     # 시작 / 끝점
-        end_point = points[-1]
-        
-        dmax = 0.0
-        index = 0
-        
-        # 매 점을 순회하며 시작점과 끝점 사이의 최대 수직 거리 계산
-        for i in range(1, len(points) - 1):
-            d = self._perpendicular_distance(points[i], start_point, end_point)
-            if d > dmax:
-                dmax = d
-                index = i
-
-        # 최대 수직 거리가 허용 오차보다 크면 재귀
-        if dmax > epsilon:
-            rec_results1 = self._rdp_simplify(points[0:index+1], epsilon)
-            rec_results2 = self._rdp_simplify(points[index:], epsilon)
-            
-            simplified_points = rec_results1[:-1] + rec_results2
-            return simplified_points
-        # 허용 오차 이내면 시작점과 끝점만 반환
-        else:
-            return [start_point, end_point]
-            
-            
-    # 위경도 -> UTM -> WGS84
-    def _convert_to_wgs84(self, utm_points: list[Point], utm_crs: pyproj.CRS) -> list[Point]:
-        # UTM -> WGS84 역변환을 위한 Transformer 생성
-        transformer = pyproj.Transformer.from_crs(utm_crs, self.crs_wgs84, always_xy=True)
-
-        lat_lon_points = []
-        for point in utm_points:
-            lon, lat = transformer.transform(point.x, point.y)
-            lat_lon_points.append(Point(lat, lon))
-            
-        return lat_lon_points
-
+    kf = KalmanFilter(
+        transition_matrices=transition_matrix,
+        observation_matrices=observation_matrix,
+        initial_state_mean=initial_state_mean,
+        observation_covariance=np.eye(2) * 1e-5,          # GPS 측정 오차
+        transition_covariance=np.eye(4) * 1e-8            # 상태 전이(예측) 오차
+    )
     
-    # 수직 거리 계산
-    def _perpendicular_distance(self, p: Point, start: Point, end: Point) -> float:
-        dx = end.x - start.x
-        dy = end.y - start.y
-        
-        if dx == 0 and dy == 0:
-            return ((p.x - start.x)**2 + (p.y - start.y)**2)**0.5
-
-        numerator = abs(dy * p.x - dx * p.y + end.x * start.y - end.y * start.x)
-        denominator = (dx**2 + dy**2)**0.5
-        
-        return numerator / denominator
+    smoothed_states_means, _ = kf.smooth(measurements)
+    smoothed_records = []
+    for i, r in enumerate(records):
+        new_record = r.copy()
+        new_record['lat'] = smoothed_states_means[i, 0]
+        new_record['lng'] = smoothed_states_means[i, 1]
+        smoothed_records.append(new_record)
+    
+    return smoothed_records, measurements, smoothed_states_means
 
 
-class JsonlIoProcessor:
-        
-    # 원본 러닝 데이터 업로드
-    def load_jsonl_data(self, file_path: str) -> List[Dict[str, Any]]:
-        records = []
-        try:
-            with open(file_path, 'r', encoding='utf-8') as f:
-                for line_num, line in enumerate(f, 1):
-                    line = line.strip()
-                    if not line:
-                        continue
-                    try:
-                        record = json.loads(line)
-                        records.append(record)
-                    except json.JSONDecodeError as e:
-                        print(f"[경고] {line_num}번째 줄에서 JSON 파싱 오류: {e}", file=sys.stderr)
-                        continue
-        except FileNotFoundError:
-            print(f"[오류] 파일을 찾을 수 없습니다: {file_path}", file=sys.stderr)
-        except Exception as e:
-            print(f"[오류] 파일 처리 중 예기치 않은 오류가 발생했습니다: {e}", file=sys.stderr)
-        return records
+# UTM 좌표계로 변환
+def to_utm(points):
+    utm_points = []
+    for i, p in enumerate(points):
+        y, x = transformer.transform(p['lat'], p['lng'])
+        utm_points.append({'x': x, 'y': y, 'original_index': i})
+    return utm_points
 
-    # 보간 데이터 저장
-    def save_data_to_jsonl(self, data: List[Any], file_path: str):
-      try:
-        with open(file_path, 'w', encoding='utf-8') as f:
-          for record in data:
-            # Point 객체일 경우 dict로 변환
-            if isinstance(record, Point):
-              json_string = json.dumps({'lat': record.x, 'lng': record.y}, ensure_ascii=False)
-            else:
-              json_string = json.dumps(record, ensure_ascii=False)
-            f.write(json_string + '\n')
-        print(f"\n[성공] 데이터가 '{file_path}' 파일에 성공적으로 저장되었습니다.")
-      except Exception as e:
-        print(f"\n[오류] 파일 저장 중 오류가 발생했습니다: {e}", file=sys.stderr)
+
+# RDP 알고리즘
+def rdp(points: list[Point], epsilon: float) -> list[Point]:
+    if len(points) < 3:
+        return points
+
+    start_point, end_point = points[0], points[-1]     # 시작 / 끝점
+    dmax, index = 0.0, 0
+    
+    for i in range(1, len(points) - 1):       # 점 - 선분 수직 거리 최대값 탐색
+        d = perpendicular_distance(points[i], start_point, end_point)
+        if d > dmax:
+            dmax = d
+            index = i
+
+    if dmax > epsilon:      # 허용 오차보다 크면 재귀
+        rec_results1 = rdp(points[0:index+1], epsilon)
+        rec_results2 = rdp(points[index:], epsilon)
+        simplified_points = rec_results1[:-1] + rec_results2
+        return simplified_points
+    else:
+        return [start_point, end_point]
+    
+def perpendicular_distance(p: Point, start: Point, end: Point) -> float:
+    dx = end.x - start.x
+    dy = end.y - start.y
+    
+    if dx == 0 and dy == 0:
+        return ((p.x - start.x)**2 + (p.y - start.y)**2)**0.5
+
+    numerator = abs(dy * p.x - dx * p.y + end.x * start.y - end.y * start.x)
+    denominator = (dx**2 + dy**2)**0.5
+    
+    return numerator / denominator    
       
+      
+# 데이터 로드/저장
+def load_jsonl_data(file_path: str) -> List[Dict[str, Any]]:
+    records = []
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            for line_num, line in enumerate(f, 1):
+                line = line.strip()
+                if not line:
+                    continue
+                try:
+                    record = json.loads(line)
+                    records.append(record)
+                except json.JSONDecodeError as e:
+                    print(f"[경고] {line_num}번째 줄에서 JSON 파싱 오류: {e}", file=sys.stderr)
+                    continue
+    except FileNotFoundError:
+        print(f"[오류] 파일을 찾을 수 없습니다: {file_path}", file=sys.stderr)
+    except Exception as e:
+        print(f"[오류] 파일 처리 중 예기치 않은 오류가 발생했습니다: {e}", file=sys.stderr)
+    return records
+
+def save_data_to_jsonl(data: List[Dict[str, Any]], file_path: str):
+    try:
+        with open(file_path, 'w', encoding='utf-8') as f:
+            for record in data:
+                json_string = json.dumps(record, ensure_ascii=False)
+                f.write(json_string + '\n')
+        print(f"\n[성공] 데이터가 '{file_path}' 파일에 성공적으로 저장되었습니다.")
+    except Exception as e:
+        print(f"\n[오류] 파일 저장 중 오류가 발생했습니다: {e}", file=sys.stderr)
+
 
 # Main
 if __name__ == '__main__':
     
     # 파일 경로 설정
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    input_file = os.path.join(script_dir, "result/smoothed_data.jsonl")
+    input_file = os.path.join(script_dir, "dummy/data1.jsonl")
     output_file = os.path.join(script_dir, "result/smoothed_data.jsonl")
 
     # 러닝 데이터 로드
-    jsonl_processor = JsonlIoProcessor()
-    running_data = jsonl_processor.load_jsonl_data(input_file)
-
-    # RDP 적용
-    point_processor = RDPPointProcessor()
+    running_data = load_jsonl_data(input_file)
     
-    # 코스 렌더링용 해상도 축소
-    epsilon_in_meters = 1.0
-    points = [Point(record['lat'], record['lng']) for record in running_data]
-    print(f"처리할 데이터 점 개수: {len(running_data)}")
+    # Kalman Filter를 활용하여 보간 및 저장
+    kalman_data, original_measurements, smoothed_measurements = smooth_gps_with_kalman(running_data)
+    save_data_to_jsonl(kalman_data, os.path.join(script_dir, "result/kalman_filtered_data.jsonl"))
+    
+    # UTM 변환
+    utm_kalman_data = to_utm(kalman_data)
+    
+    # Point 변환
+    points = []
+    for i, record in enumerate(utm_kalman_data):
+        points.append(Point(record['x'], record['y'], i))
+    
+    # 원본
+    print("\n========== 원본 ==================\n")
+    print("알고리즘 적용 전 : " + str(len(utm_kalman_data)))
+    
+    # RPD 알고리즘 적용
+    print("\n========== RDP 알고리즘 ==================\n")
     start_time = time.time()
-    simplified_track = point_processor.process_gps_track(points, epsilon_in_meters)
+    simplified_track = rdp(points, 8.0)
     end_time = time.time()
-    print(f"단순화된 데이터 점 개수 (epsilon={epsilon_in_meters}m): {len(simplified_track)}")
+    print(f"단순화된 데이터 점 개수): {len(simplified_track)}")
     print(f"처리 시간: {end_time - start_time:.4f}초")
     
     # 보간한 데이터 저장
-    jsonl_processor.save_data_to_jsonl(simplified_track, output_file)
+    result = []
+    for point in simplified_track:
+        idx = point.idx
+        result.append(kalman_data[idx])
+    save_data_to_jsonl(result, output_file)
